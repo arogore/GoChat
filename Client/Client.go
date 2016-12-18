15d24a2a9b6ee6a3a53f08a389a12731a8d57025
arogore <arongore@gmail.com>
2016-12-17 22:45:37 -0500

    Indentation changes

--- Client/Client.go
+++ Client/Client.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
-	"strings"
+  "bufio"
+  "fmt"
+  "net"
+  "os"
+  "strings"
 )
 
 var port string = "1287"
 
 /**
- *	Connects to the host and prompts for a username
- *	Starts waiting for messages and handles sending its' own 
- */
+*  Connects to the host and prompts for a username
+*  Starts waiting for messages and handles sending its' own
+*/
 func main() {
-	host := "localhost"
-	fmt.Println("Connecting to " + host + " on port " + port)
+  host := "localhost"
+  fmt.Println("Connecting to " + host + " on port " + port)
 
-	conn, err := net.Dial("tcp", host+":"+port)
-	if err != nil {
-		fmt.Println("Error connecting" + err.Error())
-		return
-	}
+  conn, err := net.Dial("tcp", host+":"+port)
+  if err != nil {
+    fmt.Println("Error connecting" + err.Error())
+    return
+  }
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter a username: ")
-	username, _ := reader.ReadString('\n')
-	fmt.Println()
-	username = strings.TrimSpace(username)
-	conn.Write([]byte(string(username)))
-	go waitMessages(conn)
+  reader := bufio.NewReader(os.Stdin)
+  fmt.Print("Enter a username: ")
+  username, _ := reader.ReadString('\n')
+  fmt.Println()
+  username = strings.TrimSpace(username)
+  conn.Write([]byte(string(username)))
+  go waitMessages(conn)
 
-	for {
-		txt, _ := reader.ReadString('\n')
-		txt = strings.TrimSpace(txt)
-		if txt == "quit" {
-			break
-		}
-		conn.Write([]byte(txt))
-	}
-	conn.Close()
+  for {
+    txt, _ := reader.ReadString('\n')
+    txt = strings.TrimSpace(txt)
+    if txt == "quit" {
+      break
+    }
+    conn.Write([]byte(txt))
+  }
+  conn.Close()
 }
 
 /**
- *	Waits for a message to be received and outputs it
- *	@param conn - Connection we're waiting to receive from
- */
+*  Waits for a message to be received and outputs it
+*  @param conn - Connection we're waiting to receive from
+*/
 func waitMessages(conn net.Conn) {
-	for {
-		var buffer [512]byte
-		len, _ := conn.Read(buffer[0:])
-		fmt.Println("\n" + string(buffer[0:len]) + "\n")
-	}
+  for {
+    var buffer [512]byte
+    len, _ := conn.Read(buffer[0:])
+    fmt.Println("\n" + string(buffer[0:len]) + "\n")
+  }
 }
--- Server/Server.go
+++ Server/Server.go
@@ -1,109 +1,109 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"strconv"
-	"strings"
+"fmt"
+"net"
+"strconv"
+"strings"
 )
 
 var last10 []string
 var connections []ConnectedUser
 
 type ConnectedUser struct {
-	conn  net.Conn
-	uName string
+conn  net.Conn
+uName string
 }
 
 /**
- *	Starts listening for connections and sends them to a handler
- */
+*	Starts listening for connections and sends them to a handler
+*/
 func main() {
-	fmt.Println("Listening on port 1287")
-	ln, err := net.Listen("tcp", ":1287")
-	if err != nil {
-		fmt.Print("Error in the connection")
-		return
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Print("Error in client connection")
-		} else {
-			connUser := addUser(conn)
-			fmt.Println("Connection amount: " + strconv.Itoa(len(connections)) + " - Connected user: " + connUser.uName)
-			go handleConnection(connUser)
-		}
-	}
+fmt.Println("Listening on port 1287")
+ln, err := net.Listen("tcp", ":1287")
+if err != nil {
+  fmt.Print("Error in the connection")
+  return
+}
+for {
+  conn, err := ln.Accept()
+  if err != nil {
+    fmt.Print("Error in client connection")
+    } else {
+      connUser := addUser(conn)
+      fmt.Println("Connection amount: " + strconv.Itoa(len(connections)) + " - Connected user: " + connUser.uName)
+      go handleConnection(connUser)
+    }
+  }
 }
 
 /**
- *	Handles a user's connection
- *	@param user - The user we want to handle
- */
+*	Handles a user's connection
+*	@param user - The user we want to handle
+*/
 func handleConnection(user *ConnectedUser) {
-	for {
-		var b [512]byte
-		n, err := user.conn.Read(b[0:])
-		if err != nil {
-			fmt.Println("Error in reading message: " + err.Error() + " from " + user.uName)
-			go broadcast(user, " has disconnected.")
-			user.conn.Close()
-			for i, u := range connections {
-				if u == *user {
-					connections = append(connections[:i], connections[i+1:]...)
-					break
-				}
-			}
-			break
-		} else {
-			fmt.Println(user.uName + ": " + string(b[:n]))
-			go broadcast(user, string(b[:n]))
-		}
-	}
+  for {
+    var b [512]byte
+    n, err := user.conn.Read(b[0:])
+    if err != nil {
+      fmt.Println("Error in reading message: " + err.Error() + " from " + user.uName)
+      go broadcast(user, " has disconnected.")
+      user.conn.Close()
+      for i, u := range connections {
+        if u == *user {
+          connections = append(connections[:i], connections[i+1:]...)
+          break
+        }
+      }
+      break
+    } else {
+      fmt.Println(user.uName + ": " + string(b[:n]))
+      go broadcast(user, string(b[:n]))
+    }
+  }
 }
 
 /**
- *	Broadcasts a message to all users except oUser
- * 	@param oUser - Original sender of the message
- *	@param msg - Message we're sending
- */
+*	Broadcasts a message to all users except oUser
+* 	@param oUser - Original sender of the message
+*	@param msg - Message we're sending
+*/
 func broadcast(oUser *ConnectedUser, msg string) {
-	for _, user := range connections {
-		if *oUser != user {
-			writeMessage(&user, oUser.uName+": "+msg)
-		}
-	}
+  for _, user := range connections {
+    if *oUser != user {
+      writeMessage(&user, oUser.uName+": "+msg)
+    }
+  }
 }
 
 /**
- *	Adds a user to the connection slice
- *	@param conn - User's connection
- *	@return connUser - New ConnectedUser
- */
+*	Adds a user to the connection slice
+*	@param conn - User's connection
+*	@return connUser - New ConnectedUser
+*/
 func addUser(conn net.Conn) *ConnectedUser {
-	connUser := ConnectedUser{conn, *retrieveUsername(conn)}
-	connections = append(connections, connUser)
-	return &connUser
+  connUser := ConnectedUser{conn, *retrieveUsername(conn)}
+  connections = append(connections, connUser)
+  return &connUser
 }
 
 /**
- *	Retrieves a username from the client
- *	@param conn - User's connection
- *	@return uname - User's username
- */
+*	Retrieves a username from the client
+*	@param conn - User's connection
+*	@return uname - User's username
+*/
 func retrieveUsername(conn net.Conn) *string {
-	var b [512]byte
-	n, _ := conn.Read(b[0:])
-	uname := strings.TrimSpace(string(b[0:n]))
-	return &uname
+  var b [512]byte
+  n, _ := conn.Read(b[0:])
+  uname := strings.TrimSpace(string(b[0:n]))
+  return &uname
 }
 
 /**
- *	Writes a message to a user
- *	@param user - The user we want to write a message to
- *	@param msg - The message we want to send
- */
+*	Writes a message to a user
+*	@param user - The user we want to write a message to
+*	@param msg - The message we want to send
+*/
 func writeMessage(user *ConnectedUser, msg string) {
-	user.conn.Write([]byte(msg))
+  user.conn.Write([]byte(msg))
 }
